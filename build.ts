93da13960e1813de2978e4d864ec15b0d4c3bf98
lovelyorg <lovelyorg@126.com>
2020-10-27 14:53:11 +0800

    完成基础功能

---
+++ build.ts
@@ -0,0 +1,36 @@
+import { walk } from "https://deno.land/std@0.74.0/fs/mod.ts";
+
+async function allFilesNames(root: string) {
+  let data = [];
+  for await (const entry of walk(root)) {
+    data.push(entry.path);
+  }
+  return data;
+}
+
+async function writeJson(path: string, data: object): Promise<string> {
+  try {
+    Deno.writeTextFileSync(path, JSON.stringify(data));
+    return "Written to " + path + " SUCCESS";
+  } catch (e) {
+    return e.message;
+  }
+}
+
+async function build() {
+  const paths = await allFilesNames("src");
+  const cate = paths.filter((m) => /^src\\[^\\]+$/.test(m)).map((m) =>
+    /[^\\]+$/.exec(m)?.toString()
+  );
+  const list = paths.filter((m) => /^src\\[^\\]+\\[^\\]+$/.test(m)).map(
+    (m) => ({
+      path: m,
+      name: m.replace(/^src\\[^\\]+\\/, ""),
+    }),
+  );
+  // console.log(cate, list);
+
+  console.log(await writeJson("./cate.json", { cate, list }));
+}
+
+build();
