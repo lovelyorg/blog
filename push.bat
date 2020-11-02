0c28ae7c87cfcc0817179ef703c130e6b4f4a33e
lovelyorg <lovelyorg@126.com>
2020-11-02 15:40:38 +0800

    'update'

---
+++ push.bat
@@ -0,0 +1,25 @@
+call ./build.bat
+
+git status
+
+pause
+
+git pull origin master
+
+pause
+
+git add -A
+
+git status
+
+pause
+
+git commit -m 'update'
+
+git status
+
+pause
+
+git push origin master
+
+pause
